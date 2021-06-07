24c58edbe79f04db62c3c0aff7c5e21394502754
nduginets <nduginets@devexperts.com>
2021-06-08 01:35:31 +0500

    optimize gradle

--- saver/src/main/kotlin/vk/saver/Main.kt
+++ saver/src/main/kotlin/vk/saver/Main.kt
@@ -1,14 +1,16 @@
+@file:JvmName("Main")
+
 package vk.saver
 
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
-    if (args.size != 2) {
+    if (args.size != 3) {
         exitProcess(1)
     }
     val host = args[0]
     val port = Integer.parseInt(args[1])
-    val dbClient = DaoClient(host, port)
-
-    dbClient.close()
+    val launcher = SaverLauncher("", host, port)
+    val future = launcher.run()
+    future.get()
 }
